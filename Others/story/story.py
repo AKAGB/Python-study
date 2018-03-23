fe85c5efab5aa1045a42fe531c55c936f830b3c4
AKAGB <374118168@qq.com>
2018-03-23 13:55:40 +0800

    story

--- Others/story/story.py
+++ Others/story/story.py
@@ -1,4 +1,5 @@
 import requests
+import os.path
 from bs4 import BeautifulSoup
 
 class StoryCrawler:
@@ -19,6 +20,12 @@ class StoryCrawler:
             html = None                                     # 若爬取失败则返回None
         return html
 
+    @staticmethod
+    def write_file(text, title):
+        """将正文写到文件中"""
+        with open('store/' + title + '.txt', 'w') as f:
+            f.write(text)
+
     def get_chapter_links(self):
         """获取章节链接"""
         html = self.get_HTML(self.url)
@@ -33,11 +40,32 @@ class StoryCrawler:
         else:
             print('获取失败！请检查输入网址。')
 
+    def read_links(self):
+        length = len(self.links)
+        if not os.path.exists('store/'):                    # 检测是否有store目录
+            os.makedirs('store')
+        for i in range(length):
+            if i > 20:                                      # 简单测试
+                break
+            print('正在爬取第 ' + str(i+1) + ' 章...')
+            html = self.get_HTML(self.links[i])
+            text = self.get_text(html)
+            self.write_file(text, self.titles[i])
+
+    def get_text(self, html):
+        """获取正文"""
+        dom = BeautifulSoup(html, 'lxml')
+        clr = dom.find(style='clear:both')
+        text = ''
+        for tag in clr.next_siblings:
+            if tag == clr:
+                break
+            text += tag.string
+        return text.replace('\xa0\xa0\xa0\xa0', '\n  ')
+
 
 if __name__ == '__main__':
     url = 'http://www.qingyunian.net/'
     sc = StoryCrawler(url)
     sc.get_chapter_links()
-    length = len(sc.titles)
-    for i in range(length):
-        print(sc.titles[i] + ' -> ' + sc.links[i])
+    sc.read_links()
